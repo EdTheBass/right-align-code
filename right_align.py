dccb0bb9e8a11d54f7d65dde39023ac0f8931851
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2021-03-08 18:07:26 +0000

    Add files via upload
    made the shitty code less shitty

--- right_align.py
+++ right_align.py
@@ -1,15 +1,15 @@
 import sys
 
 if len(sys.argv) != 3:
-    raise AttributeError("Input/output file not supplied.")
+    raise Exception("Usage: python right-align.py input-file output-file.")
 
 if not open(sys.argv[1], "r"):
-    raise FileNotFoundError("File not found.")
+    raise FileNotFoundError("Input file not found.")
 
 with open(sys.argv[1], "r") as inp_f:
     with open(sys.argv[2], "w") as out_f:
         to_write = []
         for line in inp_f.readlines():
-            ra_line = " " * (120 - len(line)) + line
+            ra_line = " " * (100 - len(line)) + line
             to_write.append(ra_line)
         out_f.writelines(to_write)
